339503373bf648e2267826763f22649024d6b9b8
sherm3 <mamafiendaasd@gmail.com>
2022-11-09 23:02:11 +0300

    firstmethod

--- Program.cs
+++ Program.cs
@@ -4,3 +4,16 @@ string? inputStrings = Console.ReadLine();
 FirstArray = inputStrings!.Split(' ');
 
 string[] SecondArray = new string[FirstArray.Length];
+
+void FillSecondArray(string[] FirstArray, string[] SecondArray)
+{
+    int i = 0;
+    for (int j = 0; j < FirstArray.Length; j++)
+    {
+        if (FirstArray[j].Length <= 3)
+        {
+            SecondArray[i] = FirstArray[j];
+            i++;
+        }
+    }
+}
